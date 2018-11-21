99f45cd14aaecf93e2d547d81ca41b4b163501b8
Роман Палкин <romangg81@gmail.com>
2018-11-21 18:50:56 +0700

    5th lesson passed

--- 5_homework/game.c
+++ 5_homework/game.c
@@ -8,8 +8,21 @@ int main(){
     srand (time(NULL));
     int n = rand() % 100 + 1;
 
-    //Пишите свою программу здесь
-
-
+    int guess;
+    int steps = 0;
+    do{
+        steps++;
+        printf("Enter your guess\n");
+        scanf("%d", &guess);
+        if(guess != n){
+            if(guess > n){
+                printf("My number is lower\n");
+            }
+            else{
+                printf("My number is higher\n");
+            }
+        }
+    }while(guess != n);
+    printf("Well done! You did it in %d steps\n", steps);
     return 0;
 }
